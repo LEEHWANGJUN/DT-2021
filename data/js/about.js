01e0ef84c6292868750c52c961ff90de22e101a2
nijoow <nijoow1127@gmail.com>
2021-10-05 16:28:42 +0900

    Revert "어바웃효과추가"
    This reverts commit 47be69e3b3ac1ef4854b85333c1f612b8e5cc09e.

--- data/js/about.js
+++ data/js/about.js
@@ -1,12 +1,13 @@
 const sectionLetterFront = document.querySelector('.section__letter-front');
+const sectionLetterEnd = document.querySelector('.section__letter-end');
 const letterFrontTop = document.querySelector('.letter-front__Top');
 const letterFrontMiddle = document.querySelector('.letter-front__Middle');
 const letterFrontBottom = document.querySelector('.letter-front__Bottom');
-const letterEndList = document.querySelectorAll('.letter-end__blur');
-const letterEndRowList = document.querySelectorAll('.letter-end-row');
-
+const letterEndMORY = document.querySelector('.letter-end__MORY');
+const letterEndRY = document.querySelector('.letter-end__RY');
 let moveFrontTop, moveFrontMiddle, moveFrontBottom;
 let moryAlpha, ryAlpha;
+
 function textAnimation(){
     moveFrontTop = (sectionLetterFront.offsetLeft - sectionLetterFront.getBoundingClientRect().left)/3;
     moveFrontMiddle = (sectionLetterFront.offsetLeft - sectionLetterFront.getBoundingClientRect().left)/10;
@@ -14,25 +15,12 @@ function textAnimation(){
     letterFrontTop.style.transform = `translateX(` + -moveFrontTop +`px)`;
     letterFrontMiddle.style.transform = `translateX(` + -moveFrontMiddle +`px)`;
     letterFrontBottom.style.transform = `translateX(` + -moveFrontBottom +`px)`;
+/*
+    console.log(sectionLetterEnd.getBoundingClientRect().left);
+    moryAlpha = 
+    letterEndMORY.style.fill = `rgba(0,0,0,` + moryAlpha + `)`;*/
+}
 
-}
-function blurOn(){
-    this.classList.add('blurOn');
-    for(let i=0; i<Math.floor(Math.random()*5+4);i++){
-    letterEndList[Math.floor(Math.random()*23)].classList.add('blurOn');
-    }
-}
-function blurOff(){
-    for(let i = 0;i<letterEndList.length;i++){
-        letterEndList[i].classList.remove('blurOn');
-    }
-}
 window.addEventListener('wheel', textAnimation,true);
 window.addEventListener('keydown',textAnimation,true);
 window.addEventListener('scroll',textAnimation,true);
-
-for(let i=0;i<letterEndList.length;i++){
-    letterEndList[i].addEventListener('mouseenter', blurOn);
-    letterEndList[i].addEventListener('mouseleave', blurOff);
-}
-
